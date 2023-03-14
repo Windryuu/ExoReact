4e194fe2927b035d53250cb5e017d579761d7c23
Brayan JEAN <brayan.jean@laposte.net>
2023-03-14 14:02:41 +0100

    small tweaks + appel function du comp parent dans enfant

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Body from './components/Body';
 import Sidebar from './components/Sidebar';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+
+// function ConsoleLogger() {
+//   console.log("console log from the parrent compo")
+// }
+
+const SuperConsoleLogger = forwardRef((props, ref) => {
+  useImperativeHandle(ref, () => ({
+    DubConsoleLogger(){
+      console.log("Je ne sais pas pourquoi j'ai eu besoin d'écrire tout ça");
+    },
+  }))
+  // return <div></div>
+})
+
+function MegaConsoleLogger() {
+  console.log("bleeep");
+}
 
 function App() {
+  const superConsoleLoggerRef = useRef(null);
   return (
+
     <div className="App">
       <Header />
-      <Sidebar />
+      <Sidebar sclr={superConsoleLoggerRef} parentfunction={MegaConsoleLogger}/>
+      <SuperConsoleLogger ref={superConsoleLoggerRef}/>
+      
       <Body />
       <Footer />
     </div>
@@ -16,3 +38,4 @@ function App() {
 }
 
 export default App;
+// export {ConsoleLogger};
